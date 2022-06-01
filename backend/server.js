835ca8db49d5281854a0579ed64767db27ee41e7
Moyuri Akther <akthermoyuri@.com>
2022-06-01 20:34:42 +0600

    heroku publish

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
-import data from "./data.js";
+import path from "path";
 import mongoose from "mongoose";
 import cors from "cors";
-// import bodyParser from "body-parser";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import seedRouters from "./routes/seedRoutes.js";
 import productRouters from "./routes/ProductRoutes.js";
@@ -22,14 +22,14 @@ mongoose
 
 const app = express();
 app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/keys/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
 app.use("/api/seed", seedRouters);
@@ -37,6 +37,12 @@ app.use("/api/products", productRouters);
 app.use("/api/user", userRouter);
 app.use("/api/orders", orderRouter);
 // console.log(orderRouter);
+
+const __dirname = path.resolve(); 
+app.use(express.static(path.join(__dirname, "/front-end/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/front-end/build/index.html"))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
